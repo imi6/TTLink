9fa9e66cf9ac57f2165f5aaae49847732bea5638
imi6 <56383294+imi6@users.noreply.github.com>
2025-05-21 15:27:52 +0800

    Update action.sh

--- action.sh
+++ action.sh
@@ -4,14 +4,9 @@ export PATH="/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH"
 
 module_dir="/data/adb/modules/TTLink"
 scripts_dir="/data/adb/TTLink/scripts"
-source ${scripts_dir}/airport.service
 
 restart_proxy_service() {
   if [ ! -f "${module_dir}/disable" ]; then
-    echo "✈️pull Airport Service"
-    airport
-    #${scripts_dir}/airport.service enable >/dev/null 2>&1
-
     echo "🔁restart TTLink"
     ${scripts_dir}/TTLink.service enable >/dev/null 2>&1
   else
